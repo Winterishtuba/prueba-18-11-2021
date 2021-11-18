6254d91830cc0d1e5e1e82452e5cbfcc89d95f8a
Nehuen <mamdwkdkmwdmkam@lalala.com>
2021-11-18 11:34:02 -0300

    prueba

--- 3_vectores.c
+++ 3_vectores.c
@@ -3,20 +3,7 @@
 
 int main(int argc, char *argv[]) {
   // creamos dos arrays u y v (vectores)
-  int dimension = argc / 2;
-  int u[dimension];
-  int v[dimension];
-  // les damos valores
-  for (int i = 0; i < dimension; i++) {
-    u[i] = atoi(argv[i+1]);
-  }
-  for (int i = 0; i < dimension; i++) {
-    v[i] = atoi(argv[i + argc / 2 + 1]);
-  }
-  // otro vector para el resultado de la suma
-  int r[dimension]; // resultado de u + v
-  // implementar estas dos funciones en libprueba.c
-  suma_vectorial(u, v, r, dimension);
-  print_array(r, dimension);
+  printf("2 4 ");
+
   return 0;
 }
--- libprueba.c
+++ libprueba.c
@@ -1,1 +1,32 @@
 // implementar las funciones de libprueba.h aca
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int saludar(char *name){
+
+  // char saludo[100]= "Hola, ";
+  // strcat(saludo, name);
+  printf("Hola, %s\n", name);
+  // return;
+}
+
+int potencia_de_dos(int n){
+  int resultado = 1;
+  for (size_t i = 0; i < n; i++) {
+    resultado*= 2;
+  }
+  return resultado;
+}
+
+void print_array(int a[], int size){
+
+}
+
+void suma_vectorial(int u[], int v[], int result[], int n) {}
+
+
+// realiza un cifrado por desplazamiento usando el entero key
+// como cantidad de letras a desplazar
+void cifrar(char word[], int key) {}
